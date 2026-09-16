a16195d0f3d32926f824e48981973ad48969d6c4
Pooja Patkar <68493624+Pooja-Patkar-Cosmo@users.noreply.github.com>
2026-09-16 22:56:08 +0530

    Add files via upload

---
+++ Exercise 03- Strings/Q6.java
@@ -0,0 +1,11 @@
+public class Q6 {
+    public static void main(String[] args) {
+        String str = "The Quick BroWn FoX!";
+        
+        System.out.println("Original String: " + str);
+        
+        String upperStr = str.toUpperCase();
+        
+        System.out.println("String in uppercase: " + upperStr);
+    }
+}
