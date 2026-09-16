58e8729a2b2cd9e3dfcc41c7d8da2f24495ac076
Pooja Patkar <68493624+Pooja-Patkar-Cosmo@users.noreply.github.com>
2026-09-16 22:52:38 +0530

    Add files via upload

---
+++ Exercise 03- Strings/Q5.java
@@ -0,0 +1,12 @@
+public class Q5 {
+    public static void main(String[] args) {
+        String str = "The quick brown fox jumps over the lazy dog.";
+        
+        System.out.println("Original string: " + str);
+        
+        // Replaces all occurrences matching regex "fox" with "cat"
+        String newStr = str.replaceAll("fox", "cat");
+        
+        System.out.println("New String     : " + newStr);
+    }
+}
